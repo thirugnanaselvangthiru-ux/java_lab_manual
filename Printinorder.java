dbc6452c13fb076ff745aa41e164a9028ebb7141
G.Thirugnanaselvan <thirugnanaselvangthiru@gmail.com>
2026-07-25 10:52:45 +0530

    Implement Foo class for ordered printing

---
+++ Printinorder.java
@@ -0,0 +1,36 @@
+class Foo {
+
+    private boolean firstDone = false;
+    private boolean secondDone = false;
+
+    public Foo() {
+
+    }
+
+    public synchronized void first(Runnable printFirst) {
+        printFirst.run();
+        firstDone = true;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (!firstDone) {
+            wait();
+        }
+
+        printSecond.run();   
+        secondDone = true;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (!secondDone) {
+            wait();
+        }
+
+        printThird.run();
+    }
+}
+
+
+ 
